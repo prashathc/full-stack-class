d27a96df8e1a2ea1ce1c0e6d09762f9ac07491d3
prashath-cheliyan <58231062+prashath-cheliyan@users.noreply.github.com>
2020-02-09 12:51:08 -0500

    middleware

--- node-express-intro-2/main.js
+++ node-express-intro-2/main.js
@@ -1,19 +1,61 @@
 const express = require('express');
+const password = 'mysecretpassword';
+
+const cups = [
+    {
+        typer: 'plastic',
+        color: 'green',
+        'size': 'L',
+    },
+    {
+        typer: 'glass',
+        color: 'blue',
+        'size': 'M',
+    },
+];
+
+const cupRouter = express.Router();
+cupRouter.route('/')
+    .get((req, res, next) => {
+        console.log(req.newField)
+        if (req.query.search) {
+            const searchCups = cups.filter((cups) => {
+                return cups.colour === req.query.search
+            })
+
+            res.json({ data: searchCups });
+            return
+        }
+
+        res.json({ data: cups })
+    })
+    .post((req, res, next) => {
+        console.log(req.body)
+        const { type, colour, size } = req.body;
+        const newCup = {
+            type,
+            colour,
+            size
+        }
+
+        cups.push(newCup);
+        res.json(newCup);
+    })
+
+
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    const { query } = req;
-    console.log(query)
-    if (query.tupac && query.tupac !== "") {
-        return res.json({ message: `tupac is da ${query.tupac}` })
+app.use((req, res, next) => {
+    // sensitive passwords/tockens go in authorization header
+    if (req.headers.authorization !== password) {
+        res.status(404).send('<h1>Wrong password</h1>');
+        return
     }
-    res.json({ message: 'great req' });
+    next();
 })
 
-app.get('/:id', (req, res, next) => {
-    const { id } = req.params
-    res.json({ message: `your id is ${id}` })
-})
-
+app.use('/cups', cupRouter)
+app.use(express.static('public'));
 app.listen('8080', console.log('listening on port 8080'));
-
