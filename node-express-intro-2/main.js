ab1b88d95aab47e278a3a298160ae9894289c6d4
prashath-cheliyan <58231062+prashath-cheliyan@users.noreply.github.com>
2020-02-09 11:47:02 -0500

    nicee

--- node-express-intro-2/main.js
+++ node-express-intro-2/main.js
@@ -5,10 +5,14 @@ app.get('/', (req, res) => {
     const { query } = req;
     console.log(query)
     if (query.tupac && query.tupac !== "") {
-        res.json({ message: `tupac is da ${query.tupac}` })
+        return res.json({ message: `tupac is da ${query.tupac}` })
     }
     res.json({ message: 'great req' });
+})
 
+app.get('/:id', (req, res, next) => {
+    const { id } = req.params
+    res.json({ message: `your id is ${id}` })
 })
 
 app.listen('8080', console.log('listening on port 8080'));
